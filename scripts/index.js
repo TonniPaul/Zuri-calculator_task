3db693aa0aa169017659408285e0ac7b24c191fc
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-06-24 21:13:51 +0100

    restructured code.

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,9 @@
 let select = prompt("What mathimatical function would you like to perform, Reply with +, -, *, / 0r %");
 let maths = ["+", "-", "*","/","%"];
-let numberOne;
-let numberTwo;
-let sum;
+let numberOne, numberTwo, sum;
 if (select === maths[0]){
    numberOne = prompt("Number One", );
-   numberTwo = prompt("Number Two", )
+   numberTwo = prompt("Number Two", );
    sum = Number(numberOne + numberTwo);
    alert(`${numberOne} ${select} ${numberTwo}= ${sum}`);
 }else if(select === maths[1]){
