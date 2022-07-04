20ad79293c4739c811a78957ada0f905480f8c2b
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-04 18:54:24 +0100

    Refacrored code

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,30 @@ let select = prompt("What mathimatical function would you like to perform, Reply
 let maths = ["+", "-", "*","/","%"];
 let numberOne, numberTwo, sum;
 if (select === maths[0]){
-   numberOne = prompt("Number One", );
-   numberTwo = prompt("Number Two", );
-   sum = Number(numberOne + numberTwo);
+   console.log(select)
+   numberOne = parseInt(prompt("Number One", ));
+   numberTwo = parseInt(prompt("Number Two", ));
+   sum = numberOne + numberTwo;
    alert(`${numberOne} ${select} ${numberTwo}= ${sum}`);
 }else if(select === maths[1]){
-   numberOne = prompt("Number One", );
-   numberTwo = prompt("Number Two", );
+   console.log(select)
+   numberOne = parseInt(prompt("Number One", ));
+   numberTwo = parseInt(prompt("Number Two", ));
    sum = Number(numberOne - numberTwo);
    alert(`${numberOne} ${select} ${numberTwo}= ${sum}`);
 }else if(select === maths[2]){
-   numberOne = prompt("Number One", );
-   numberTwo = prompt("Number Two", );
+   numberOne = parseInt(prompt("Number One", ));
+   numberTwo = parseInt(prompt("Number Two", ));
    sum = Number(numberOne * numberTwo);
    alert(`${numberOne} ${select} ${numberTwo}= ${sum}`);
 }else if(select === maths[3]){
-   numberOne = prompt("Number One", );
-   numberTwo = prompt("Number Two", );
+   numberOne = parseInt(prompt("Number One", ));
+   numberTwo = parseInt(prompt("Number Two", ));
    sum = Number(numberOne / numberTwo);
    alert(`${numberOne} ${select} ${numberTwo}= ${sum}`);
 }else if(select === maths[4]){
-   numberOne = prompt("Number One", );
-   numberTwo = prompt("Number Two", );
+   numberOne = parseInt(prompt("Number One", ));
+   numberTwo = parseInt(prompt("Number Two", ));
    sum = Number(numberOne % numberTwo);
    alert(`${numberOne} ${select} ${numberTwo}= ${sum}`);
 }else{
